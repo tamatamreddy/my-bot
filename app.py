27f43c4c1bba716ff19b28594ba11fda8cf58e71
tamatamreddy <118552966+tamatamreddy@users.noreply.github.com>
2025-01-04 18:00:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("Tamatam Bot")
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "GPT-4o"
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
